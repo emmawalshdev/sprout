8c98acc895637ed31981ff357366c6bbb07f3a0f
Emma Walsh <walshemma33@outlook.com>
2025-08-31 20:21:38 +0100

    Replace input with chatbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import InputBox from './components/chatbot/inputBox'
+import Chatbox from './components/chatbot/chatbot'
 
 function App() {
   
   return (
     <>
-    <InputBox></InputBox>
+    <Chatbox/>
     </>
   )
 }
--- src/components/chatbot/chatbot.jsx
+++ src/components/chatbot/chatbot.jsx
@@ -1,11 +1,56 @@
 // main chatbox
-import React from "react";
+import React, {useState} from "react";
+import sendMessageToApi from "../../api/chat";
 
-const chatbox = () => {
+const Chatbox = () => {
+    const [input, setInput] = useState('');
+    const [botReply, setBotReply] = useState('')
+
+    const [messages, setMessages] = useState([]);
+
+    const handleSubmit = async(e) => {
+        e.preventDefault();
+        if(!input.trim()){return;}
+
+        // 1. add users message
+        const timeNow = new Date().toLocaleTimeString();
+        setMessages([...messages, {user: 'Emma', text: input, time: timeNow}]);
+
+        // call api
+        sendMessageToApi(input);
+        const botReply = await sendMessageToApi(input);
+
+        setMessages((prev) => [
+            ...prev,
+            {user: 'Sprout', text: botReply, time: timeNow}
+        ])
+    }
+
     return (
-        TEST
+        <>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                value={input}
+                className="bg-green-200 p-2 rounded-xl max-w-xs"
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Type your plant question..."/>
+            <button type="submit">Send</button>
+        </form>
+        {botReply && 
+        <p>
+            {botReply}
+        </p>}
+        {messages.map((msg, index) => (
+            <p key={index}>
+                <span>Text: {msg.text}</span> <br />
+                <span>User: {msg.user}</span> <br />
+                <span>Time: {msg.time}</span> <br />
+            </p>
+        ))}
+        </>
     )
 }
 
 
-export default chatbox
+export default Chatbox
