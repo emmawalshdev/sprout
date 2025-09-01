9854a910d9b36c4f8ce8a6933eeaabbb8821dfbc
Emma Walsh <walshemma33@outlook.com>
2025-09-01 20:28:25 +0100

    Add messages styles

--- src/components/chatbot/messageList.jsx
+++ src/components/chatbot/messageList.jsx
@@ -5,15 +5,27 @@ const MessageList = ({messages, botReply}) => {
         <>
         hello
         {botReply && 
-        <p>
+        <div>
             {botReply}
-        </p>}
+        </div>}
         {messages.map((msg, index) => (
-            <p key={index}>
-                <span>Text: {msg.text}</span> <br />
-                <span>User: {msg.user}</span> <br />
-                <span>Time: {msg.time}</span> <br />
-            </p>
+            <div 
+                key={index}
+                className={`flex ${msg.user === "Emma" ? "justify-end" : "justify-start"} mb-2`}
+                >
+                <div 
+                    className={`px-4 py-2 rounded-xl max-w-xs breakwords
+                    ${msg.user === "Emma" ? "bg-green-500 text-white" : "bg-gray-200 text-gray-800"}`}
+                >
+                    <div className="flex flex-start mt-2">
+                        {msg.text}
+                    </div>
+                    <div className="flex flex-row justify-start space-x-1 text-gray-600 text-[10px]">
+                        <span>Sent by {msg.user}</span> 
+                        <span>@ {msg.time}</span> 
+                    </div>
+                </div>
+            </div>
         ))}
         </>
     )
